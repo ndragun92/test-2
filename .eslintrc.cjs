8493398d9c0b58c128959924fe402e48a7fa9b3c
ndragun92 <nemanjadragun92@gmail.com>
2024-03-08 12:36:25 +0100

    fix: tests

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  ignorePatterns: ['dist/', 'node_modules/'],
+  ignorePatterns: ['dist/', 'node_modules/', 'src/**.spec.ts'],
   extends: 'standard-with-typescript',
   overrides: [
     {
@@ -16,12 +16,12 @@ module.exports = {
       parserOptions: {
         sourceType: 'script'
       }
-    }
+    },
   ],
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
   },
   rules: {
-  }
+  },
 }
