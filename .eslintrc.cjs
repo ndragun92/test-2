2ea1873e1286f3a5dbeeddc3b72e57a36b52f8c4
ndragun92 <nemanjadragun92@gmail.com>
2024-03-08 13:02:14 +0100

    chore: test beta

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  ignorePatterns: ['dist/', 'node_modules/'],
+  ignorePatterns: ['dist/', 'node_modules/', 'src/**.spec.ts'],
   extends: 'standard-with-typescript',
   overrides: [
     {
--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,14 @@ import dts from 'vite-plugin-dts'
 // https://vitejs.dev/guide/build.html#library-mode
 export default defineConfig({
   build: {
+    manifest: true,
+    minify: true,
+    reportCompressedSize: true,
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'test-2',
-      fileName: 'test-2'
+      fileName: 'test-2',
+      formats: ['es', 'cjs', 'umd']
     }
   },
   plugins: [dts()]
